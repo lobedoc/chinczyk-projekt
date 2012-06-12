d8c22dc30a2830b22fb77eddfed7bc7dfca7aa60
mateusz.mikolajczak89 <mateusz.mikolajczak89@gmail.com>
2012-06-12 12:10:28 +0000

    +poruszanie sie

--- branches/LudoPackage/src/com/ludo/game/other/Pawn.java
+++ branches/LudoPackage/src/com/ludo/game/other/Pawn.java
@@ -6,7 +6,13 @@ import javax.swing.JComponent;
 
 public class Pawn extends JComponent implements Serializable{
 
-	private int blockId;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6728120083942141197L;
+	private int blockId; //pozycja startowa danego pionka
+	private int actualyPosition = 0;
+	private int targetPosition = 0;
 	
 	public Pawn(){
 		
@@ -17,5 +23,34 @@ public class Pawn extends JComponent implements Serializable{
 	public int getBlockId(){
 		return blockId;
 	}
+	public boolean canMove(int movement){
+		targetPosition = (actualyPosition + movement)%63;
+		if(targetPosition > blockId)
+			return false;
+		return true;
+	}
+	public boolean move(int movement){
+		if(!canMove(movement)){
+			targetPosition=actualyPosition - blockId;
+			targetPosition +=68;
+			targetPosition -=2;
+			targetPosition -=64;
+			//redCamp.camp(targetPosition);
+			return true;
+		}
+		else{
+		targetPosition = (actualyPosition + movement)%63;
+		moveTo(targetPosition);
+		return true;
+		}
+	}
 	
+	private void moveTo(int movement){
+		//wyrysowanie pionka do poruszania sie
+	}
+	private void moveFromBase(int movement) {
+		if (movement != 6) {
+			 moveTo(blockId);
+		}
+	}
 }
